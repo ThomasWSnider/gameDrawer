7cc6ec3ed5197380ad51271c9414185f6be1697d
ThomasWSnider <thomas.wayne.snider@gmail.com>
2025-02-28 17:44:04 -0700

    added logic for draw scenarios, game board no longer updates when game is over

--- src/components/TicTacToeBoard.vue
+++ src/components/TicTacToeBoard.vue
@@ -1,19 +1,33 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { Appstate } from "../Appstate";
 
 const players = ref(['X', 'O'])
 const currentPlayer = ref(false)
+const playerSymbol = computed(() => players.value[Number(currentPlayer.value)])
+const gameStates = ref([`inProgress`, `${playerSymbol} Wins!`, `It's a Draw!`])
+const currentGameState = ref(0)
 const board = ref({ cells: Array(9).fill("") } as { cells: string[] })
 
 function handleClick(cellIndex: number) {
-  const playerSymbol = players.value[Number(currentPlayer.value)]
+  if (currentGameState.value != 0) return
+
   if (board.value.cells[cellIndex] === "") {
-    board.value.cells[cellIndex] = playerSymbol
+    board.value.cells[cellIndex] = playerSymbol.value;
   }
-  const winnerIs = checkForWinner(playerSymbol)
-  if (winnerIs) console.log(`${currentPlayer.value} Wins!`)
-  currentPlayer.value = !currentPlayer.value
+  const winnerIs = checkForWinner(playerSymbol.value);
+
+  if (winnerIs) {
+    currentGameState.value = 1;
+    console.log(`${currentPlayer.value} Wins!`);
+  }
+
+  if (!winnerIs && !board.value.cells.includes("")) {
+    currentGameState.value = 2
+    console.log("It's a Tie!")
+  }
+
+  currentPlayer.value = !currentPlayer.value;
   return
 }
 
@@ -22,10 +36,9 @@ function checkForWinner(playerSymbol: string) {
   const gameBoard = board.value.cells;
   for (const combination of winStates) {
     const [a, b, c] = combination;
-    if (gameBoard[a] === playerSymbol && gameBoard[b] === playerSymbol && gameBoard[c] === playerSymbol)
-      return true
+    if (gameBoard[a] === playerSymbol && gameBoard[b] === playerSymbol && gameBoard[c] === playerSymbol) return true;
   }
-  return false
+  return false;
 }
 
 
